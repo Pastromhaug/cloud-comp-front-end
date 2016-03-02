88ae154e4d0b21ea49b86774327a6b0eecdf40fa
Peter Stromhaug <pas282@cornell.edu>
2016-03-01 22:54:28 -0500

    first commit, plyloader not found

---
+++ src/index.js
@@ -0,0 +1,50 @@
+
+// scene, camera, rendering setup
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+// Create VRControls in addition to FirstPersonVRControls.
+//var vrControls = new THREE.VRControls(camera);
+var fpVrControls = new THREE.FirstPersonVRControls(camera, scene);
+//var effect = new THREE.VREffect(renderer);
+//effect.setSize(window.innerWidth, window.innerHeight);
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// lighting
+var light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
+//variables
+var plz_wrk;
+
+var loader = new THREE.PLYLoader();
+loader.load( './models/plz_wrk.ply', function ( geometry ) {
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, shininess: 400, vertexColors: THREE.VertexColors} );
+    var mesh = new THREE.Mesh( geometry, material );
+    plz_wrk = mesh;
+
+    scene.add( mesh );
+    plz_wrk.rotation.x += -Math.PI / 2;
+    plz_wrk.rotation.z += Math.PI / 2 + .5;
+ } );
+
+// Optionally enable vertical movement.
+fpVrControls.verticalMovement = true;
+function animate (timestamp) {
+    // Update FirstPersonControls after VRControls.
+    // FirstPersonControls requires a timestamp.
+    //vrControls.update();
+    fpVrControls.update(timestamp);
+}
+
+camera.position.z = 5;
+
+function render() {
+    requestAnimationFrame( animate );
+	requestAnimationFrame( render );
+    plz_wrk.rotation.z += 0.001;
+    renderer.render( scene, camera );
+}
+render();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by perandre on 2/23/16.
+ */
+
+module.exports = {
+    entry: {
+        javascript: "./src/index.js",
+        html: "./src/index.html"
+    },
+    output: {
+        path: __dirname +"/dist",
+        filename: "./bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]"
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    }
+};
